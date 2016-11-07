40df7785397211e42e382ac8711e082b594455e1
RoBareiro <rsb.bareiro@live.com.ar>
2016-11-06 22:44:24 -0300

    Cambios de php
    index.php se le agrego linea comentada para resolver problema de sesion
    
    pag/index.php (estaba en la carpeta pag, no va ahi, fue eliminado)
    
    pag/login.php (solo se valida e ingresa usuario, no se maneja sesion,
    fue eliminada esa parte)

--- pag/login.php
+++ pag/login.php
@@ -2,12 +2,6 @@
 		error_reporting(0);	/*Desactiva cualquier notificacion*/
 		include("../inc/conexionbd.php");
 
-		if($_SESSION["registrado"] == "true")
-		session_start();
-		$_SESSION["registrado"]= "true";
-		require_once("../inc/seguridad.php");
-
-
 		
 		$nombre = $_POST["nombre"];
 
